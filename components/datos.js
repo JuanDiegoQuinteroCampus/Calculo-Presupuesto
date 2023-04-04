4ab93c2445230b2096d59101aac62b5b27d2564d
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-04-04 16:19:41 -0500

    feat: :ambulance: arreglo boton
    se le reparo el boton para acceder mas facilmente y yesta en proceso

---
+++ components/datos.js
@@ -0,0 +1,10 @@
+export default {
+
+    total:{
+        usuario:{
+            num:"",
+            desc:""
+        }
+    }
+    
+}
--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-function changeText() {
+/* import mydatos from "./components/datos.js"; */
+
+/* mydatos. */
+
+
+let formulario = document.querySelector('#formulario');
+
+let suma = document.querySelector("#userinput")
+
+
+
+ formulario.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log(formulario)
+
+
+
+
+})
+/* function changeText() {
     let userInput = document.getElementById("userInput").value;
     document.getElementById("outputText").innerHTML = userInput;
   }
+let change = document.querySelector("#change")
+function changeText() {
+    let userInput = document.getElementById("userInput").value;
+    document.getElementById("outputText").innerHTML = userInput;
+  } */
+  
