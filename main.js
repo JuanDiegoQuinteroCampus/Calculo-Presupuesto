199bd114e8ad8e0a1ae974fadcd634858a49181b
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-04-11 16:19:39 -0500

    feat: :sparkles: Calculo y operaciones
    Se le agregaron la funciones de operaciones pero tiene un error que separa la operacion de suma y resta en ves de juntarla

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
 /* import mydatos from "./components/datos.js"; */
 
 /* mydatos. */
+/* import config from "./config.js"; */
 
 
-let formulario = document.querySelector('#formulario');
+/* let ingresos = 0;
+let egresos = 0; 
+let total = 0;
+let select = [];
+  let desc = [];
+  let userInput = []; */
+  /* let data = []; */
+  let data = {
+      
+      ingresos:[],
+      egresos: [],
+      operaciones:[],
+  };
 
-let suma = document.querySelector("#userinput")
 
+let getData =  () => {
+  desc = document.getElementById("desc").value;//esto n hace nada por ahora
+ 
+ 
 
-
- formulario.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    console.log(formulario)
-
-
-
-
-})
-/* function changeText() {
-    let userInput = document.getElementById("userInput").value;
-    document.getElementById("outputText").innerHTML = userInput;
+  let userInput = parseInt(document.getElementById("userInput").value) ;
+  let select = document.getElementById("selectOption");
+  if (select.value==='Ingresos') {
+    data.ingresos.push(userInput)
+    data.operaciones.push(userInput)
+    let suma = data.operaciones.reduce(function(total,num){
+      return total + num;
+    });
+    console.log(suma);
+    document.getElementById("outputText").innerHTML = suma.toLocaleString();
   }
-let change = document.querySelector("#change")
-function changeText() {
-    let userInput = document.getElementById("userInput").value;
-    document.getElementById("outputText").innerHTML = userInput;
-  } */
-  
+  if (select.value==='Egresos'){
+    data.egresos.push(userInput)
+    data.operaciones.push(userInput)
+    let resta = data.operaciones.reduce(function(total,num){
+      return total - num;
+    });
+    console.log(resta);
+    document.getElementById("outputText").innerHTML = resta.toLocaleString();
+  }
+  console.log(data);
+
+}
+
+
+
