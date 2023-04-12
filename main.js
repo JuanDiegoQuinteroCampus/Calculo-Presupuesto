f1c74b726eacb3d4db85a91a6de9cac1951bf999
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-04-11 22:10:26 -0500

    feat: :sparkles: Arreglo en tablas
    Se le hizo un arreglo a las tablas y se le añadio un evento para cuando ponga la descripcion y el valor aparesca en la tabla segun si es ingreso o egreso; worker en proceso

--- main.js
+++ main.js
@@ -4,14 +4,6 @@
 /* import config from "./config.js"; */
 
 
-/* let ingresos = 0;
-let egresos = 0; 
-let total = 0;
-let select = [];
-  let desc = [];
-  let userInput = []; */
-  /* let data = []; */
-
   let data = {
     ingresos:[],
     egresos: [],
@@ -35,6 +27,7 @@ let select = [];
       }, 0);
       console.log(resultado);
       document.getElementById("outputText").innerHTML = resultado.toLocaleString();
+      document.getElementById("outIngreso").innerHTML = suma.toLocaleString();
     }
     if (select.value === 'Egresos') {
       data.egresos.push(userInput)
@@ -47,10 +40,48 @@ let select = [];
       }, 0) - resta;
       console.log(resultado);
       document.getElementById("outputText").innerHTML = resultado.toLocaleString();
+      document.getElementById("outEgreso").innerHTML = resta.toLocaleString();
     }
     console.log(data);
   }
+
+
+  function agregarDato(event) {
+    event.preventDefault(); // Previene que se recargue la página
+    
+    // Obtiene los valores ingresados ​​en el formulario
+    const option = document.getElementById("selectOption").value;
+    const desc = document.getElementById("desc").value;
+    const userInput = document.getElementById("userInput").value;
+    
+    // Crea una nueva fila en la tabla con los datos ingresados
+    const nuevaFila = `
+      <tr>
+        <td class="d-flex justify-content-around bg-light px-3 border">
+          <p><span>${desc}</span></p>
+          <p class="${option === 'Ingresos' ? 'text-success' : 'text-danger'}">${option === 'Ingresos' ? '$+' : '$-'}<span>${userInput}</span></p>
+        </td>
+      </tr>
+    `;
+  
+    // Agrega la nueva fila a la tabla correspondiente
+    if (option === 'Ingresos') {
+      const tablaDatos = document.getElementById("tabla-datos");
+      tablaDatos.insertAdjacentHTML("beforeend", nuevaFila);
+    } else if (option === 'Egresos') {
+      const tablaDatos = document.getElementById("tabla-datos2");
+      tablaDatos.insertAdjacentHTML("beforeend", nuevaFila);
+    }
+  
+    // Limpia los valores del formulario
+    document.getElementById("selectOption").value = "";
+    document.getElementById("desc").value = "";
+    document.getElementById("userInput").value = "";
+  }
+  
+  // Agrega un listener al formulario para capturar el evento submit y llamar a la función agregarDato()
+  const formulario = document.getElementById("formulario");
+  formulario.addEventListener("submit", agregarDato);
   
 
-
-
+  
